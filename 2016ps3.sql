bf741c9594cc72a7b3000f329e39b952eb1249ac
Eric Ehmann <eaehmann@gmail.com>
2017-04-05 13:14:19 -0400

    Cleaned up code

--- 2016ps3.sql
+++ 2016ps3.sql
@@ -9,9 +9,7 @@
 --
 -- There are 6 tables on this DB
 --
--- Author:  Maria R. Garcia Altobello
---
--- Student:  <<Eric Ehmann>>
+-- Author:  <<Eric Ehmann>>
 --
 -- Date:   October, 2016
 --
@@ -48,59 +46,58 @@ DROP sequence seq_shipment;
 
 CREATE table tbProduct (
         prodNo          char(3)                 not null
-        CONSTRAINT pk_tbProduct primary key,
+        	CONSTRAINT pk_tbProduct PRIMARY KEY,
         productDesc     varchar2(15)            not null,
         schedule        number(2,0)             not null
 );
 
 CREATE table tbPart (
         partNo          char(2)                 not null
-         constraint pk_tbPart primary key CHECK (partNo between '01' and '99'),
+        	CONSTRAINT pk_tbPart PRIMARY KEY CHECK (partNo between '01' and '99'),
         partDesc       varchar2(15)            not null,
         quantityOnHand  number(8,0)             not null
-        CONSTRAINT rq_quantityOnHand CHECK (quantityOnHand between '0' and '1000')
+        	CONSTRAINT rq_quantityOnHand CHECK (quantityOnHand between '0' and '1000')
 );
 
 CREATE table tbVendor (
         vendorNo        char(3)                 not null
-        constraint pk_tbVendor primary key CHECK (vendorNo between '100' and '999'),
+        	CONSTRAINT pk_tbVendor PRIMARY KEY CHECK (vendorNo between '100' and '999'),
         vendorName      varchar2(25)            not null,
         vendorCity      varchar2(15)            null        
 );
 
 CREATE table tbComponent (
         prodNo          char (3)                not null
-        	CONSTRAINT fk_prodNo_tbComponent references tbProduct (prodNo) on delete cascade,
+        	CONSTRAINT fk_prodNo_tbComponent REFERENCES tbProduct (prodNo) on delete cascade,
         compNo          char (2)                not null
-         constraint rg_compNo CHECK (compNo between '01' and '10'),
+        	CONSTRAINT rg_compNo CHECK (compNo between '01' and '10'),
         partNo          char (2)                null
         	CONSTRAINT fk_partNo_tbComponent references tbPart (partNo),
         noPartsReq      number (2,0)            default 1     not null
-         CONSTRAINT rg_noPartReq CHECK (noPartsReq between '1' and '10'),        
-        CONSTRAINT PK_tbComponent PRIMARY KEY(prodNo, CompNo)
+         	CONSTRAINT rg_noPartReq CHECK (noPartsReq between '1' and '10'),        
+        	CONSTRAINT PK_tbComponent PRIMARY KEY(prodNo, CompNo)
 );
 
 CREATE table tbQuote (
         vendorNo        char(3)                 not null
-        constraint fk_vendorNo_tbQuote references tbVendor (vendorNo),
+        	CONSTRAINT fk_vendorNo_tbQuote references tbVendor (vendorNo),
         partNo          char(2)                 not null
-        constraint fk_partNo_tbQuote references tbPart (partNo) on delete cascade,
+        	CONSTRAINT fk_partNo_tbQuote references tbPart (partNo) on delete cascade,
         priceQuote      number(11,2)            default 0     not null,
-        CONSTRAINT PK_tbQuote PRIMARY KEY(vendorNo, partNo)
+        	CONSTRAINT PK_tbQuote PRIMARY KEY(vendorNo, partNo)
 );
 
 
 CREATE table tbShipment (
         shipmentNo      number (11,0)           not null
-        CONSTRAINT pk_tbShipment primary key,
+        	CONSTRAINT pk_tbShipment PRIMARY KEY,
         vendorNo        char (3)                not null,
         partNo          char (2)                not null,
         quantity        number (4,0)            default 1
-        constraint rq_tbShipment check (quantity between '1' and '1000'),
-        shipmentDate    date                    default CURRENT_DATE     not null,
-        CONSTRAINT FK_tbQuote FOREIGN KEY (vendorNo, partNo)
-        REFERENCES tbQuote(vendorNo, partNo)
-        ON DELETE CASCADE        
+        	CONSTRAINT rq_tbShipment CHECK (quantity between '1' and '1000'),
+        shipmentDate    date  					default CURRENT_DATE     not null,
+        	CONSTRAINT FK_tbQuote FOREIGN KEY (vendorNo, partNo)
+        	REFERENCES tbQuote(vendorNo, partNo)on delete cascade        
 );
 
 
@@ -186,8 +183,7 @@ SELECT * FROM tbVendor;
 -- ******************************************************
 --    QUALITY CONTROLS
 --
--- Note:  Test only 2 constraints of each of
---        the following types:
+-- Demonstrate integrity tests
 --        *) Entity integrity
 INSERT into tbProduct values (100, 'Shovel', 2);
 INSERT into tbShipment values (null,'909', '01', 2, to_date('10-01-2016','mm-dd-yyyy') );
@@ -204,7 +200,7 @@ INSERT into tbVendor values ('1', 'FirstOne', 'Boston');
 --    *) Triggers
 -- ******************************************************
 CREATE or REPLACE trigger TR_new_ship_IN
-   /* trigger executes BEFORE an INSERT into the GIFT table */
+   /* trigger executes BEFORE an INSERT into the SHIPMENT table */
    before insert on tbShipment
    /* trigger executes for each ROW being inserted */
    for each row
@@ -225,19 +221,20 @@ CREATE or REPLACE trigger TR_new_ship_IN
 
 CREATE OR REPLACE trigger two_quote_min_rule
    after insert or update or delete on tbPart   
-   declare
+   DECLARE
       x number; /* PL/SQL variable */  
-   begin
+   BEGIN
       /* projects with no tasks considered */
-      select count(pricequote) into x
-         from tbQuote 
-         group by partNo;                         
+      SELECT count(pricequote) into x
+      FROM tbQuote 
+      GROUP BY partNo;                         
+      
       if x > 0 then
          dbms_output.put_line (
             '*** Warning, you must have at least two quotes - 
             two_quote_min_rule TRIGGER ');
       end if;
-       end two_quote_min_rule;
+	end two_quote_min_rule;
 /
 -- *****************************************************
 -- Queries
@@ -255,39 +252,41 @@ SELECT partDesc
 --- Which products require the use of a box?
 SELECT productDesc 
     FROM tbProduct     
-    	WHERE prodNo IN(SELECT prodNo
+    WHERE prodNo IN(SELECT prodNo
     						FROM tbComponent
     						WHERE partNo='97');
+
 ---Query3
 -- What Parts are only sold by one vendor
-	SELECT P.partDesc, Q.partNo, count(Q.priceQuote) 
-		FROM tbQuote Q, tbPart P
-		WHERE Q.partNo=P.partNo
-		GROUP BY Q.partNo, P.partDesc
-		ORDER By 2, 1;
+SELECT P.partDesc, Q.partNo, count(Q.priceQuote) 
+	FROM tbQuote Q, tbPart P
+	WHERE Q.partNo=P.partNo
+	GROUP BY Q.partNo, P.partDesc
+	ORDER By 2, 1;
+
 --Query4
 -- What vendor offers the lowest price?
-	Select  tbVendor.Vendorname, tbQuote.partNo, tbQuote.priceQuote
-		FROM tbQuote
-		LEFT Join tbQuote cheaperQuote on tbQuote.partNo =cheaperQuote.partNo
-		AND tbQuote.priceQuote >cheaperQuote.pricequote
-		Join tbVendor on tbQuote.vendorNo = tbVendor.vendorNo
-		WHERE cheaperQuote.priceQuote is null
-		ORDER by tbQuote.partNO;
+SELECT  tbVendor.Vendorname, tbQuote.partNo, tbQuote.priceQuote
+	FROM tbQuote
+	LEFT JOIN tbQuote cheaperQuote on tbQuote.partNo =cheaperQuote.partNo
+	AND tbQuote.priceQuote >cheaperQuote.pricequote
+	JOIN tbVendor on tbQuote.vendorNo = tbVendor.vendorNo
+	WHERE cheaperQuote.priceQuote is null
+	ORDER BY tbQuote.partNO;
 		
 --Query5 
 -- What parts are being used for each product.
-	SELECT secList.prodNo, secList.compNo, tbComponent.partNO,
+SELECT secList.prodNo, secList.compNo, tbComponent.partNO,
 	tbPart.partDesc
 	FROM tbComponent
-	FULL OUTER JOIN tbComponent secList ON tbComponent.compNo = secList.compNo
+	FULL OUTER JOIN tbComponent secList on tbComponent.compNo = secList.compNo
 	LEFT JOIN tbPart on secList.partNo = tbPart.partNo
 	AND tbComponent.partNo = tbPart.partNo
-	Order by tbComponent.partNo;
+	ORDER BY tbComponent.partNo;
 	
 --Query6 
 -- Which Vendors are located in the same city
-	SELECT tbVendor.vendorName, location.vendorName, tbVendor.vendorCity
+SELECT tbVendor.vendorName, location.vendorName, tbVendor.vendorCity
 	From tbVendor
 	JOIN tbVendor location on tbVendor.vendorCity = location.vendorCity
 	WHERE tbVendor.vendorNo< location.vendorNo
@@ -296,7 +295,7 @@ SELECT productDesc
 --Query 7 
 -- How many weeks before inventory runs out
 	
-	SELECT tbPart.partDesc,
+SELECT tbPart.partDesc,
 	SUM (tbPart.quantityOnHand)/((tbComponent.noPartsReq)* (tbProduct.schedule)) as Weeks_Left
 	FROM tbPart
 	--FROM tbComponent
